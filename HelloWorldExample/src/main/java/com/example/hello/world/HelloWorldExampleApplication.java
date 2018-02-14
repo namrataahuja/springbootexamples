2a6b4a4c3b59560044cd6f93c568d8b7e095325a
Ahuja <668450@cognizant.com>
2018-02-14 14:47:10 +0530

    annotation changes

--- HelloWorldExample/src/main/java/com/example/hello/world/HelloWorldExampleApplication.java
+++ HelloWorldExample/src/main/java/com/example/hello/world/HelloWorldExampleApplication.java
@@ -1,10 +1,16 @@
 package com.example.hello.world;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
+@ComponentScan
+@EnableAutoConfiguration
 @SpringBootApplication
 public class HelloWorldExampleApplication extends SpringBootServletInitializer {
 	

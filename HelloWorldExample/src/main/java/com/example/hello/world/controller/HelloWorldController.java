eeed1344fe3f4bdd127acb7bb1dddb0b48951247
Ahuja <668450@cognizant.com>
2018-02-21 13:16:46 +0530

    updating message

--- HelloWorldExample/src/main/java/com/example/hello/world/controller/HelloWorldController.java
+++ HelloWorldExample/src/main/java/com/example/hello/world/controller/HelloWorldController.java
@@ -1,14 +1,18 @@
 package com.example.hello.world.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
 	
+	@Value("${message}")
+	String msg;
+	
 	@RequestMapping("/hello")
 	public String display() {
-		return "HELLO-WORLD";
+		return "HELLO-WORLD - "+msg;
 	}
 
 }
